cb28a8b284ea16f92bdd222ea638d08a68ca548b
GrandDynamo <43609220+GrandDynamo@users.noreply.github.com>
2020-03-21 23:14:02 +0100

    Cleaning up the file that contains routes.

--- server/api/v1/apiHandler.php
+++ server/api/v1/apiHandler.php
@@ -6,13 +6,10 @@ use classes\routing\{Request, Router};
 spl_autoload_register(function ($class) {
     require str_replace("\\", '/', "" . $class) . '.php';
 });
+
+
+
 $router  = new Router(new Request());
-
-
-// $router->get('/dataprocessing/api/v1/animes/{num}/wow/{idk}', function () {
-//     return "Routed into option: <b>1</b>";
-// });
-
 
 //Used to inject the object into the callback function.
 $apiFactory = new APIFactory();
@@ -37,33 +34,3 @@ $router->get('/dataprocessing/api/v1/animes/{num}/wow/{idk}/wow', function () us
 $router->get('/dataprocessing/api/test/{nice}', function () use ($apiFactory) {
     return "Routed into option: <b>8</b>";
 });
-
-
-
-
-
-
-// $apiFactory->getXMLFromQuery("SELECT
-// count(user_id) AS Total, 
-// (SELECT Count(gender)FROM userlist WHERE gender = 'Male') AS Male, 
-// (SELECT Count(gender) FROM userlist WHERE  gender = 'Female') AS Female 
-// FROM `userlist` 
-// GROUP BY Male, Female");
-
-// $query2 = "SELECT ratinglist.anime_id, COUNT(ratinglist.user_id) AS watched, AVG(ratinglist.rating) AS rating, animelist.name
-// FROM `ratinglist` JOIN animelist ON ratinglist.anime_id = animelist.anime_id
-// GROUP BY ratinglist.anime_id 
-// ORDER BY `watched` DESC
-// LIMIT ?";
-// $apiFactory->getXMLFromQuery($query2, 10);
-
-// $apiFactory->getXMLFromQuery("SELECT name FROM animelist WHERE anime_id > 1 LIMIT ?", 10);
-
-// $apiFactory->getJSONFromQuery("getUserRatings", 10044);
-
-
-// $apiFactory->getJSONFromQuery("getTopWatchedAnime", 12);
-// $apiFactory->getJSONFromQuery("addAnime");
-
-
-// $apiFactory->printJSONFromQuery("totalMaleFemaleWeebs");
